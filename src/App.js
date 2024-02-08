277e0a2457f05b429a18a2922773d59a19985051
Andrew Alsing <andrew32alsing@gmail.com>
2024-02-08 14:12:24 -0800

    added countdown before showing swatches

--- src/App.js
+++ src/App.js
@@ -6,16 +6,35 @@ function App() {
   const [targetColor, setTargetColor] = useState("");
   const [message, setMessage] = useState("");
   const [selectedColor, setSelectedColor] = useState("");
+  const [showSwatches, setShowSwatches] = useState(false);
+  const [countdown, setCountdown] = useState(3);
 
   useEffect(() => {
     resetGame();
   }, []);
+
+  useEffect(() => {
+    if (!showSwatches) {
+      setCountdown(3);
+      const interval = setInterval(() => {
+        setCountdown((currentCountdown) => {
+          if (currentCountdown <= 1) {
+            clearInterval(interval);
+            setShowSwatches(true);
+            return 0;
+          }
+          return currentCountdown - 1;
+        });
+      }, 1000);
+    }
+  }, [showSwatches]);
 
   const generateRandomColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
   };
 
   const resetGame = () => {
+    setShowSwatches(false);
     const newColors = Array.from({ length: 3 }, generateRandomColor);
     setColors(newColors);
     setTargetColor(newColors[Math.floor(Math.random() * newColors.length)]);
@@ -34,22 +53,26 @@ function App() {
 
   return (
     <div className="App">
-      <div className="colorWrapper">
-        {colors.map((color, index) => (
-          <div
-            key={index}
-            className={`colorSwatch ${
-              message === "Incorrect :(" && color === targetColor
-                ? "correctSwatch"
-                : ""
-            }`}
-            style={{ backgroundColor: color }}
-            onClick={() => handleSwatchClick(color)}
-          >
-            {message && <span className="colorSwatchSpan">{color}</span>}
-          </div>
-        ))}
-      </div>
+      {showSwatches ? (
+        <div className="colorWrapper">
+          {colors.map((color, index) => (
+            <div
+              key={index}
+              className={`colorSwatch ${
+                message === "Incorrect :(" && color === targetColor
+                  ? "correctSwatch"
+                  : ""
+              }`}
+              style={{ backgroundColor: color }}
+              onClick={() => handleSwatchClick(color)}
+            >
+              {message && <span className="colorSwatchSpan">{color}</span>}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Get ready... {countdown}</p>
+      )}
       <p>Click the swatch that matches: {targetColor}</p>
       <p>{message}</p>
       <button className="playAgainButton" onClick={resetGame}>
